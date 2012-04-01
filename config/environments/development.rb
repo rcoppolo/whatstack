efea27b457a8f4a3e22da0ab551aed0d30e87a29
Ryan Coppolo <rcoppolo22@gmail.com>
2012-04-01 00:46:22 -0700

    Sets up devise and password forget.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -27,8 +27,9 @@ Whatstack::Application.configure do
   config.assets.debug = true
   
   config.action_mailer.delivery_method = :smtp
+  config.action_mailer.default_url_options = { :host => 'localhost:1025' }
   config.action_mailer.smtp_settings = { :address => "localhost", :port => 1025 }
-  config.action_mailer.perform_deliveries = false
+  config.action_mailer.perform_deliveries = true
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default :charset => "utf-8"
 end

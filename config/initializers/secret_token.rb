979d2d2691718f425db9ab16f5e2b7b13672996b
Ryan Coppolo <r@coppolo.com>
2012-04-01 00:02:42 -0700

    Initial commit.

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Whatstack::Application.config.secret_token = '05c4040e3a8cf4aed4d00a00c28bf6536b5d951f79d41c5de57923d01d15488ed0d16d9e863bc0458ac7d3e755bb66fe86fe13060482fd26f7f60edb8dbc2f40'
